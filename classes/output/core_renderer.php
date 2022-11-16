01dc35f42c368f6b80cd03c14f827725b36eb613
Trevor Cunningham <trevor.cunningham@uregina.ca>
2022-11-16 14:10:01 -0600

    brings in loginform customizations

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -553,6 +553,28 @@ class core_renderer extends \theme_boost\output\core_renderer {
 	}
 	
 	
+    /**
+     * Renders the login form.
+     *
+     * @param \core_auth\output\login $form The renderable.
+     * @return string
+     */
+    public function render_login(\core_auth\output\login $form) {
+        global $CFG, $SITE;
+
+        $context = $form->export_for_template($this);
+
+        $context->errorformatted = $this->error_text($context->error);
+        $url = $this->get_logo_url();
+        if ($url) {
+            $url = $url->out(false);
+        }
+        $context->logourl = $url;
+        $context->sitename = format_string($SITE->fullname, true,
+                ['context' => context_course::instance(SITEID), "escape" => false]);
+
+        return $this->render_from_template('theme_boost_union/loginform', $context);
+    }
 	
 	
 }
